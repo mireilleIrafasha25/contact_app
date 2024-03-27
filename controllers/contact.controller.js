bfffbc81d0cc9921eee94dea2ed14552c2485559
mireilleIrafasha25 <156303827+mireilleIrafasha25@users.noreply.github.com>
2024-03-27 18:40:33 +0200

    First commit

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,1 +1,92 @@
-// Contact controllers here
+import contactModel from "../models/contact.model.js";
+ 
+
+const contactController=
+{
+create:async(req,res)=>
+{
+    const addedcontact=await contactModel.create(req.body)
+try{
+    res.status(201).json({
+        message:'contact added successfully',
+        contact:addedcontact
+    })
+}
+catch(error)
+{
+console.log(error.message)
+res.status(500).json({
+    message:"Internal server Error"
+})
+}
+},
+list:async(req,res)=>
+{
+    const contacts=await contactModel.find()
+try{
+    res.status(200).json({
+        contact:contacts
+    })
+}
+catch(error)
+{
+console.log(error.message)
+res.status(500).json({
+    message:"Internal server Error"
+})
+}
+},
+GetbyPhone:async(req,res)=>
+{
+    const phone1=req.params.phone
+    const contacts=await contactModel.findOne({phone:phone1})
+try{
+    res.status(200).json({
+        contact:contacts
+    })
+}
+catch(error)
+{
+console.log(error.message)
+res.status(500).json({
+    message:"Internal server Error"
+})
+}
+},
+Updatecontact:async(req,res)=>
+{
+    const updatedcontact=await contactModel.findByIdAndUpdate(req.params.id,req.body,{set:true})
+try{
+    res.status(200).json({
+        message:'updated successfully',
+        contact:updatedcontact
+    })
+}
+catch(error)
+{
+console.log(error.message)
+res.status(500).json({
+    message:"Internal server Error"
+})
+}
+},
+Delete:async(req,res)=>
+{
+    const deletedcontacts=await contactModel.findByIdAndDelete(req.params.id)
+try{
+    res.status(200).json({
+        message:'deleted successfully',
+        contact:deletedcontacts
+    })
+}
+catch(error)
+{
+console.log(error.message)
+res.status(500).json({
+    message:"Internal server Error"
+})
+}
+}
+}
+export default contactController;
+
--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,1 +1,12 @@
 // Contact routes here
+import contactController from "../controllers/contact.controller.js";
+import express from 'express';
+const router=express.Router()
+
+router.post('/create',contactController.create)
+router.get('/list',contactController.list)
+router.get('/getbyphone/:phone',contactController.GetbyPhone)
+router.put('/update/:id',contactController.Updatecontact)
+router.delete('/delete/:id',contactController.Delete)
+
+export default router;
